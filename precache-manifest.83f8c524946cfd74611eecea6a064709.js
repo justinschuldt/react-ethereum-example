51733afe5df877036b519cd52b00806865ecffbe
Justin Schuldt <justinschuldt@gmail.com>
2019-05-21 16:45:36 -0500

    Updates

---
+++ precache-manifest.83f8c524946cfd74611eecea6a064709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1ceb807bb9bf13209dbd0925f84f726",
+    "url": "/static/media/Wallet.e1ceb807.sol"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "437cf6db4ca0ee819eaf",
+    "url": "/static/js/main.437cf6db.chunk.js"
+  },
+  {
+    "revision": "96225c30f5eac7b7d7d9",
+    "url": "/static/js/1.96225c30.chunk.js"
+  },
+  {
+    "revision": "437cf6db4ca0ee819eaf",
+    "url": "/static/css/main.12d14c19.chunk.css"
+  },
+  {
+    "revision": "878dac0f4cc0589fd98b74099dfd9dc5",
+    "url": "/index.html"
+  }
+];
